07abaa3180b9b6df6a76fb71fe19a9b19d2d7a27
christopher-christofi <christopherlchristofi@outlook.com>
2022-04-30 23:14:40 +0100

    add new environment variables for message email creation

--- initbuild.py
+++ initbuild.py
@@ -4,9 +4,13 @@ def configauth(path=None):
     sender = input("Dedicated sender email address: ")
     recip = input("Recipient email address (leave blank if same as Sender): ") or sender
     passw = getpass.getpass(prompt='Password for Sender email address: ')
+    msg = input("Message (leave blank for default): ") or "Loglistener says Hello."
+    subj = input("Subject Line (leave blank for default): ") or "Log Message" 
 
-    with open(str(path) + "/.loglistenerproject.env", "a") as f:
+    with open(str(path), "+w") as f:
         f.write("SENDER=%s\n" % (sender))
         f.write("RECIPIENT=%s\n" % (recip))
         f.write("PASSW=%s\n" % (passw))
+        f.write("MESSAGE=%s\n" % (msg))
+        f.write("SUBJECT=%s\n" % (subj))
     return 0
