3105693e1aaf76524d723eebc27e64ca8c3446a5
christopher-christofi <christopherlchristofi@outlook.com>
2022-07-26 21:26:14 +0100

    Add gmail api integration for full email send service, functional run option and env provision

--- test_send.py
+++ test_send.py
@@ -1,60 +1,60 @@
-import os
-import smtplib
+import os, base64
+import google_util
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 # dev dependencies
 import dotenv
 import pathlib
 
-def read_climate():
+CLIENT_SECRET_FILE = 'client_secret.json'
+API_NAME = 'gmail'
+API_VERSION = 'v1'
+SCOPES = ['https://mail.google.com/']
+
+def read_env():
     # TODO to be based on env file not dotenv lib
     sender = os.getenv('SENDER')
     recipient = os.getenv('RECIPIENT')
-    password = os.getenv('PASSW')
     subject = os.getenv('SUBJECT')
     message = os.getenv('MESSAGE')
-    return sender, recipient, password, subject, message
+    return sender, recipient, subject, message
 
 def email_meta():
-    
-    sender_email, recipient_email, passw, subj, msg = read_climate()
-    
+
+    sender_email, recipient_email, subj, msg = read_env()
+
     email_meta = {
         'sender': sender_email,
         'target': recipient_email,
-        'passw': passw,
         'message': msg,
         'subject': subj
     }
     return email_meta
 
-def start_serve():
+def start_send_service():
+
+    service = google_util.create_service(
+            CLIENT_SECRET_FILE,
+            API_NAME,
+            API_VERSION,
+            SCOPES
+    )
 
     msg = MIMEMultipart()
 
     struct_email = email_meta()
-    password = struct_email.get('passw')
-    msg['From'] = struct_email.get('sender')
     msg['To'] = struct_email.get('target')
     msg['Subject'] = struct_email.get('subject')
     msg.attach(MIMEText(struct_email.get('message'), 'plain'))
-
-    server = smtplib.SMTP('smtp.gmail.com: 587')
-
-    server.starttls()
-
-    server.login(msg['From'], password)
-
-    server.sendmail(['From'], msg['To'], msg.as_string())
-
-    server.quit()
-
+    raw_data_string = base64.urlsafe_b64encode(msg.as_bytes()).decode()
+    email_message = service.users().messages().send(userId=struct_email.get('sender'), body={'raw': raw_data_string}).execute()
     print("email should have been sent to: %s" % (struct_email.get('target')))
 
 def serve_mail(path):
+    print(path)
     # dev
     dotenv.load_dotenv(dotenv_path=pathlib.Path(path))
-    start_serve()
+    start_send_service()
     return 0
 
 if __name__ == '__main__':
